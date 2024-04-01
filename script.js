c67a12657f175b1d8a5d9089f4ad4fb3f447d724
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-03-31 20:47:34 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".navbar-toggler");
+  const dropdown = document.querySelector(".sub-menu");
+
+  toggleBtn.addEventListener("click", () => {
+    dropdown.classList.toggle("open");
+  });
+
+  
+ const header = document.querySelector("#header");
+ const navbar = document.querySelector("#navbar");
+ const body = document.querySelector("body");
+
+ 
+  
+});
